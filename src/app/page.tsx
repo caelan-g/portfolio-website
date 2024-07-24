0ed24e49656501fe0069fa97f96360cf9f5d076b
caelan-g <141709327+caelan-g@users.noreply.github.com>
2024-07-24 22:31:24 +1000

    images, layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 "use client";
 import { useScroll, useTransform } from "framer-motion";
 import React from "react";
+import Image from "next/image";
 import { FlipWords } from "./components/ui/flip-words";
 
 export default function Home() {
-  const words = [
-    "Hey",
-    "Salut",
-    "Anyoung",
-    "Nǐ hǎo",
-    "G'day",
-    "¿Qué tal?",
-    "Privet",
-  ];
+  const words = ["Hey,", "Salut", "Nǐ hǎo", "G'day,", "Hola", "Privet"];
 
   return (
     <>
@@ -23,7 +16,7 @@ export default function Home() {
             I'm Caelan
           </div>
           <div className="text-xl pt-3 text-neutral-400 w-3/6">
-            Year 11 student based in Sydney, Australia. Enjoys learning and
+            Year 11 student based in Sydney, Australia. I enjoy learning and
             creating with the newest tech and software.
           </div>
           <div id="work" className="text-5xl pt-36 pb-72">
@@ -31,7 +24,14 @@ export default function Home() {
               id="custom-bento"
               className="grid grid-cols-6 grid-rows-4 w-ful h-[100vh] gap-3"
             >
-              <div className="bento-container col-span-2"></div>{" "}
+              <div className="bento-container col-span-2">
+                <Image
+                  src="/images/CJlogocompany.png"
+                  alt="Clipjiffy logo"
+                  width="100"
+                  height="100"
+                />
+              </div>{" "}
               {/*could map these?*/}
               <div className="bento-container col-span-1"></div>
               <div className="bento-container row-span-2 col-span-1"></div>
